5f35455629fd3dc358e1bb1678aae66341318d89
Kaloyan KRASTEV <83513233+kaloyansen@users.noreply.github.com>
2021-06-07 09:05:01 +0200

    Add files via upload

--- MockServiceApi.js
+++ MockServiceApi.js
@@ -19,85 +19,79 @@ function fetchMock(url) {
     }
 }
 
-
-function takeOne(x, y) { return (Math.random() > 0.5) ? x : y; }
-function testTakeOne(loopmax = 1e7) {
-    /* takeOne(x, y) standart test
-       to see if it is really random
-       it is now tested and
-       it looks very random */
-    let dif = tot = 0;
-    console.log('please wait while verifying random takeOne(x, y) ...');
-    while (tot < loopmax) {
-        dif += (takeOne(true, false)) ? 1 : -1;
-        tot ++;
-    }
-    console.log('random deviation:', dif / tot);
-
-}
-
-
-
 const logInParams = { method: 'POST', body : { email: 'hello@email.com', password: 'salut123@' } };
-function logIn(params) {
+let logIn = params => new Promise(function(resolve, reject) {
     /* @endpoint: /logIn
        @method: POST
        @params: { email: <text>, password:<text> }
        @response: { success: <boolean>, user: {name: <text>, avatar_url:<string>} } */
-    return new Promise(function(resolve, reject) {
-        testTakeOne(1e8);
-        const data = takeOne(
-            { success: true, user: { name: "toto", avatar_url: 'https://serveur.fr/avatars/toto1234.jpg' } },
-            { success: false, user: { name: null, avatar_url: null } });
-        setTimeout(() => { resolve(data); }, timeout);
-    });
-}
-
+    const data = takeOne(
+        { success: true, user: { name: "toto", avatar_url: 'https://serveur.fr/avatars/toto1234.jpg' } },
+        { success: false, user: { name: null, avatar_url: null } });
+    setTimeout(() => { resolve(data); }, timeout);
+});
 
 const sendMessageParams = { method: 'POST', body : { text: 'some text', timestamp: '12748', recipient_id: '326'} };
-function sendMessage(params) {
+let sendMessage = params => new Promise(function(resolve, reject) {
     /* @endpoint: /sendMessage
        @method: POST
        @params: { text: <text>, timestamp:<number>, recipient_id:<number> }
-       @response: { success: <boolean>, delivered: <boolean>} } */
-    return new Promise(function(resolve, reject) {
-        const data = takeOne(
-            { success: true, delivered: false },
-            { success: false, delivered: true });
-        setTimeout(() => { resolve(data); }, timeout * 2);
-    });
-}
+       @response: { success: <boolean>, delivered: <boolean>} } */  
+    const data = takeOne(
+        { success: true, delivered: false },
+        { success: false, delivered: true });
+    setTimeout(() => { resolve(data); }, timeout * 2);
+});
 
 const newMessageParams = { method: 'GET', body : {} };
-function newMessage(params) {
+let newMessage = params => new Promise(function(resolve, reject) {
     /* @endpoint: /newMessage
        @method: GET
        @params: {}
        @response: { success: <boolean>, from: <number>} } */
-    return new Promise(function(resolve, reject) {
-        const data = takeOne(
-            { success: true, from: 1234566 },
-            { success: false, from: null });
-        setTimeout(() => { resolve(data); }, timeout * 3);
-    });
-}
-
+    const data = takeOne(
+        { success: true, from: 1234566 },
+        { success: false, from: null });
+    setTimeout(() => { resolve(data); }, timeout * 3);
+});
 
 const loadNewMessagesParams = { method: 'POST', body : { id_new_messages: '1024' } };
-function loadNewMessages(params) {
+let loadNewMessages = params => new Promise(function(resolve, reject) {
     /* @endpoint: /loadNewMessages
        @method: POST
        @params: { id_new_messages: <number> }
        @response: { success: <boolean>, nb_message: <number>, messages: [{timestamp:<number>, body:<text> }] } } */
-    return new Promise(function(resolve, reject) {
-        const data = takeOne(
-            { success: true, nb_message: 3, messages: [ { timestamp: 13456, body: "Yo, ça va ?" },
-                                                        { timestamp: 13477, body: "Moi ça va, et toi ?" },
-                                                        { timestamp: 13499, body: "Oui, moi aussi." }, ] },
-            { success: false, nb_message: 0, messages: null });
-        setTimeout(() => { resolve(data); }, timeout * 4);
-    });
+    const data = takeOne(
+        { success: true, nb_message: 3, messages: [ { timestamp: 13456, body: "Yo, ça va ?" },
+                                                    { timestamp: 13477, body: "Moi ça va, et toi ?" },
+                                                    { timestamp: 13499, body: "Oui, moi aussi." }, ] },
+        { success: false, nb_message: 0, messages: null });
+    setTimeout(() => { resolve(data); }, timeout * 4);
+});
+
+
+let sciExpo = (x, f = 1) => Number.parseFloat(x).toExponential(f);
+let takeOne = (x, y) => (Math.random() > 0.5) ? x : y;
+function takeOneTest(loopmax = 6e6) {
+    /* takeOne(x, y) standart test
+       to see if it is really random
+       it is now tested and
+       it looks very random */
+    let dif = tot = div = 0;
+    console.log('please wait while verifying random takeOne(x, y) ...');
+    while (tot < loopmax) {
+        dif += (takeOne(true, false)) ? 1 : -1;
+        tot ++;
+        div = dif / tot;
+        if (tot % (loopmax / 80)) {
+            tot = tot;
+        } else {
+            console.clear();
+            console.log(`(fast random test) statistic: ${sciExpo(tot)}/${sciExpo(loopmax)}, deviation from zero: ${sciExpo(div)}`);
+        }
+    }
+
+    return div;
 }
 
-module.exports = { fetchMock }
-
+module.exports = { fetchMock, takeOneTest }
--- client.js
+++ client.js
@@ -1,20 +1,26 @@
-let { fetchMock } = require ('./MockServiceApi');
+/* usage:
+   node client.js */
+
+//import { testTakeOne, fetchMock } from './MockServiceApi.mjs';
+const { fetchMock, takeOneTest } = require('./MockServiceApi.js');
+//let { testTakeOne, fetchMock } = require ('./MockServiceApi');
 
 /* Consignes:
    Dans le fichier MockServiceApi.js, vous suivrez les indications en commentaires pour écrire des fonctions de mock de données.
    Chaque fonctions devra retourner un JSON qui correspond à la réponse prévu.
    Ces fonction doivent permettre de simuler une requête http à une api distante.
-   Elles doivent donc retourner une promesse avec un certain temps de latence dans avant d'être résolue.
+   Elles doivent donc retourner une promesse avec un certain temps de latence dans avant d'être résolue. */
 
-   usage:
-   node client.js */
+takeOneTest(1e8);
 
-let p; // custom promise
-console.clear();
-
-// gonna test all endpoints
-p = newPromise(`/logIn`);
-delete p;
+/* after having tested my random function
+   see takeOne(x, y) in ./MockServiceApi
+   i'm gonna test all endpoints
+   it is amazing that they are running in paralel
+   so they start together but anyone could finish first
+   because pending time (temps de latence) depends on random numbers */
+let p = newPromise(`/logIn`); // custom promise
+delete p; // as a c++ progammer i have learned that every <new> must have a <delete>
 p = newPromise(`/sendMessage`);
 delete p;
 p = newPromise(`/newMessage`);
@@ -23,18 +29,17 @@ p = newPromise(`/loadNewMessages`);
 delete p;
 
 function newPromise(params) {
-    let log = 'endpoint: ' + params;
-    let pro = fetchMock(params);
-
+    let log = `'${params}':`;
+    let pro = fetchMock(params); // new promise
     pro.catch(err => { console.error(err) })
     pro.then(data => {
-        if (!data.success) {// resend if no success
+        if (!data.success) { // if not (pseudo)success the function calls itself to retry ...
             console.log(log + ' pending ...');
             delete pro;
             newPromise(params);
-        } else {// success !!!
-            console.log(log + ' received.');
-            pro.then(ok => { console.log(ok) })
+        } else { // ... while success !!!
+            log += ' la réponse reçue =';
+            pro.then(ok => { console.log(log, ok, "\n") })
         }
     })
 
